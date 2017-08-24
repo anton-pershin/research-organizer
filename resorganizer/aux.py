61d855452ce4c5e9f618905b5b5a79f6af6c893d
Anton Pershin <tony.pershin@gmail.com>
2017-08-24 14:59:37 +0100

    Fixed relative paths in TaskExecution classes. Added necessary funcs in aux

--- resorganizer/aux.py
+++ resorganizer/aux.py
@@ -4,8 +4,26 @@ import os
 import os.path
 import shutil
 import re
+import numpy as np
+
+def create_file_mkdir(filepath):
+    """Opens a filepath in a write mode (i.e., creates/overwrites it). If the path does not exists,
+    subsequent directories will be created.
+    """
+    dirpath = os.path.dirname(filepath)
+    if not os.path.exists(dirpath):
+        os.makedirs(dirpath)
+    return open(filepath, 'w')
+
+
+def get_templates_path():
+    """Returns the absolute path to templates directory. It is useful when the module is imported from elsewhere.
+    """
+    return os.path.join(os.path.dirname(os.path.dirname(__file__)), 'templates')
 
 def append_code(obj, obj_funcs, code_appendix):
+    """Adds the code defined by the function code_appendix in the end of the method obj_funcs of the object obj.
+    """
     def extended_func(func, *args, **kwds):
         func(*args, **kwds)
         code_appendix(*args, **kwds)
@@ -17,11 +35,17 @@ def append_code(obj, obj_funcs, code_appendix):
         setattr(obj, func_name, partial(extended_func, func))
 
 def is_sequence(arg):
+    """Checks whether arg is a sequence (string does not count as a sequence)
+    """
     return (not hasattr(arg, "strip") and
             hasattr(arg, "__getitem__") or
             hasattr(arg, "__iter__"))
 
 def do_atomic(proc_func, cleanup_func):
+    """Executes the function proc_func such that if an expection is raised, the function cleanup_func
+    is executes and only after that the expection is hand over further. It is useful when proc_func
+    creates something which should be removed in the case of emergency.
+    """
     try:
         proc_func()
     except Exception as err:
@@ -29,23 +53,29 @@ def do_atomic(proc_func, cleanup_func):
         raise err
 
 def make_atomic(proc_func, cleanup_func):
+    """Returns a function corresponding to do_atomic() to which proc_func and cleanup_func are passed.
+    """
     return partial(do_atomic, proc_func, cleanup_func)
 
 def cp(from_, to_):
+    """
+    Copy from_ to to_ where from_ may be file or dir and to_ is a dir.
+    """
     if os.path.isfile(from_):
         shutil.copy(from_, to_)
     else:
         shutil.copytree(from_, to_)
 
 def rm(target):
+    """Remove target which may be file or dir.
+    """
     if os.path.isfile(target):
         os.remove(target)
     else:
         shutil.rmtree(target)
 
 def merge_dicts(*dict_args):
-    """
-    Given any number of dicts, shallow copy and merge into a new dict,
+    """Given any number of dicts, shallow copy and merge into a new dict,
     precedence goes to key value pairs in latter dicts.
     Source: Aaron Hall, https://stackoverflow.com/questions/38987/how-to-merge-two-dictionaries-in-a-single-expression
     """
@@ -54,16 +84,72 @@ def merge_dicts(*dict_args):
         result.update(dictionary)
     return result
 
-def find_dir_by_named_regexp(where, regexp):
+def find_dir_by_named_regexp(regexp, where):
+    """Search for dir in where which satisfies regexp. If successful, parses the dir according to named regexp.
+    Returns a tuple (found_dir, params_from_named_regexp) or None if not found.
+    """
     dirnames = next(os.walk(where))[1]
     for dir_ in dirnames:
         parsing_params = parse_by_named_regexp(regexp, dir_)
         if parsing_params is not None:
-            return parsing_params
+            return dir_, parsing_params
     return None
 
 def parse_by_named_regexp(regexp, val):
+    """Parses val according to named regexp. Return a dictionary of params.
+    """
     matching = re.search(regexp, val)
     if matching is None:
         return None
     return matching.groupdict()
+
+def parse_datafile(path, data_names, transform_funcs, cols_to_parse=[]):
+    """Parses a data file given by path and structured as a table where rows are separated by \n
+    and columns are separated by any of whitespaces. The first line in the file will be ignored.
+    Processed columns are given by cols_to_parse (all columns will be processed if it is empty).
+    Corresponding names and transformation functions for columns in cols_to_parse are given by 
+    data_names and transform_funcs. Transformation function must be a mapping string -> type.
+    
+    Returns a dictionary where a key corresponds to a column name (i.e., taken from data_names)
+    and a value corresponds to a list of the columns values taken from all rows.
+    """
+    if cols_to_parse == []:
+        cols_to_parse = range(len(data_names))
+    if len(data_names) != len(transform_funcs) or len(data_names) != len(cols_to_parse):
+        raise Exception('Number of data names, transform functions and columns to be parsed is inconsistent')
+    data = {}
+    for data_name in data_names:
+        data[data_name] = []
+
+    f = open(path, 'r') # if not found, expection will be raised anyway
+    lines = f.readlines()
+    for line in lines[1:]: # skip the first line
+        tmp = line.split()
+        if len(tmp) < len(data_names):
+            raise Exception('Number of given data names is larger than number of columns we have in the data file.')
+        for i, data_name in enumerate(data_names):
+            val = tmp[cols_to_parse[i]]
+            data[data_name].append(transform_funcs[i](val))
+    return data
+
+def parse_timed_numdatafile(path):
+    """Parses a data file given by path and structured as a table where rows are separated by \n
+    and columns are separated by any of whitespaces. The table here has an interpretation of a matrix whose 
+    rows axis corresponds to time axis and columns axis corresponds to data axis. Moreover, the first column
+    contains the time values so the data is contained in columns starting from the second one.
+
+    Returns time_list (a list of times from the first column) and data_matrix (a list of numpy arrays of data where
+    list's index corresponds to the time index). 
+    """
+    time = []
+    data = []
+    f = open(path, 'r') # if not found, expection will be raised anyway
+    lines = f.readlines()
+    for line in lines[1:]: # skip the first line
+        tmp = line.split()
+        time.append(float(tmp[0]))
+        timed_data = np.zeros((len(tmp) - 1, ))
+        for i, val in enumerate(tmp[1:]):
+            timed_data[i] = float(val)
+        data.append(timed_data)
+    return time, data
--- resorganizer/task_execution.py
+++ resorganizer/task_execution.py
@@ -1,5 +1,6 @@
 from mako.template import Template
-from resorganizer.aux import append_code
+from resorganizer.aux import append_code, get_templates_path, create_file_mkdir
+import os
 
 class TaskExecution(object):
     """Any TaskExecution must finally produce a single command to be executed and a list of copies
@@ -8,7 +9,7 @@ class TaskExecution(object):
         self.command = None
         self.copies_list = []
         self.host_relative_copies_list = []
-        self.is_global_command = None
+        self.is_global_command = False
         append_code(self, ('set_alone_task', 'set_plural_task', 'set_chain_task'), self._add_program)
 
     def set_alone_task(self, task):
@@ -22,10 +23,7 @@ class TaskExecution(object):
 
     def _add_program(self, task):
         if task.program != '':
-            self.is_global_command = False
             self.host_relative_copies_list.append(task.program)
-        else:
-            self.is_global_command = True
 
 class DirectExecution(TaskExecution):
     def __init__(self):
@@ -33,55 +31,56 @@ class DirectExecution(TaskExecution):
 
     def set_alone_task(self, task):
         sid, self.command = next(task.command_gen())
+        self.command = './' + self.command
         self.copies_list = task.inputs
 
 class SgeExecution(TaskExecution):
     def __init__(self):
         super(SgeExecution, self).__init__()
 
-    def set_properties(cores, time):
+    def set_properties(self, cores, time):
         self.cores = cores
         self.time = time
 
     def set_alone_task(self, task):
         sid, cmd = next(task.command_gen())
+        cmd = './' + cmd
+        print(cmd)
         sge_script_filename = '{}.sh'.format(sid)
         sge_script_path = os.path.join('tmp', sge_script_filename)
         sge_data = _render_sge_template(sge_script_path, self.cores, self.time, [cmd])
         self.copies_list.append(sge_script_path)
         self.copies_list += task.inputs
         self.command = 'qsub {}'.format(sge_script_filename)
+        self.is_global_command = True
 
-    def set_plural_task(task):
+    def set_plural_task(self, task):
         # prepare sge scripts and add them into the list of copies
-        cmds = []
-        sids = []
         sges = []
         for sid, cmd in task.command_gen():
-            cmds.append(cmd)
-            sids.append(sid)
             sge_script_filename = '{}.sh'.format(sid)
             sge_script_path = os.path.join('tmp', sge_script_filename)
-            sge_data = _render_sge_template(sge_script_path, self.cores, self.time, [cmd])
+            sge_data = _render_sge_template(sge_script_path, self.cores, self.time, ['./' + cmd])
             self.copies_list.append(sge_script_path)
             sges.append(sge_script_filename)
 
         # prepare py-handler
-        handler_templ_file = open(os.path.join('templates', 'plural_task_handler.py'), 'r')
+        handler_templ_file = open(os.path.join(get_templates_path(), 'plural_task_handler.py'), 'r')
         rendered_data = Template(handler_templ_file.read()).render(max_sge_tasks_in_queue=20, sleeping_time_sec=600, sges=sges)
         handler_script_filename = 'handler.py'
         handler_script_path = os.path.join('tmp', handler_script_filename)
-        handler_script_file = open(handler_script_path, 'w')
+        handler_script_file = create_file_mkdir(handler_script_path)
         handler_script_file.write(rendered_data)
         self.copies_list.append(handler_script_path)
         self.copies_list += task.inputs
-        self.command = 'nohup python ' + self._py_script_name + ' >/dev/null 2>&1 &'
+        self.command = 'nohup python {} > handler.err 2>&1 &'.format(handler_script_filename)
+        self.is_global_command = True
 
-    def set_chain_task(task):
+    def set_chain_task(self, task):
         cmds = []
         sids = []
         for sid, cmd in task.command_gen():
-            cmds.append(cmd)
+            cmds.append('./' + cmd)
             sids.append(sid)
         for i in range(len(sids)):
             sge_script_filename = '{}.sh'.format(sids[i])
@@ -93,9 +92,10 @@ class SgeExecution(TaskExecution):
             self.copies_list.append(sge_script_path)
         self.copies_list += task.inputs
         self.command = 'qsub {}.sh'.format(sids[0])
+        self.is_global_command = True
 
 def _render_sge_template(sge_script_path, cores, time, commands):
-        sge_templ_file = open(os.path.join('templates', 'sge_script.sh'), 'r')
+        sge_templ_file = open(os.path.join(get_templates_path(), 'sge_script.sh'), 'r')
         rendered_data = Template(sge_templ_file.read()).render(cores=cores, time=time, commands=commands)
-        sge_script_file = open(sge_script_path, 'w')
+        sge_script_file = create_file_mkdir(sge_script_path)
         sge_script_file.write(rendered_data)
